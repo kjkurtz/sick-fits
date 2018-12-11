e605c6c53278cacf2b455d470c6dcf5e42ebf72f
Kyle Kurtz <kkurtz@ellingsoncompanies.com>
2018-12-11 14:31:52 -0600

    Pagination through 23

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -8,6 +8,30 @@ const Mutations = {
       }
     }, info);
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args }
+    // remove id from updates (we dont want to update it)
+    delete updates.id
+    // run update
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      },
+    },
+      info
+    )
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id }
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`)
+    // check if they own that item or have permissions to it
+    // TODO
+    // delete it
+    return ctx.db.mutation.deleteItem({ where }, info)
   }
 };
 
